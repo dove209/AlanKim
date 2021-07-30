027b154e7ca8343754012960e6a03960763d00d6
dove209 <53247748+dove209@users.noreply.github.com>
2021-07-30 14:02:38 +0900

    Update imges.js

--- server/routes/api/imges.js
+++ server/routes/api/imges.js
@@ -22,7 +22,7 @@ const upload = multer({
 //남길랭 작성이 업로드한 사진 불러오기
 router.get('/:imgName', function (req, res) {
   let imgName = (req.params.imgName);
-  fs.readFile(__dirname + `../../../uploads/${imgName}`, function (err, data) {
+  fs.readFile(__dirname + `../../uploads/${imgName}`, function (err, data) {
     res.writeHead(200, { 'Content-Type': 'image/jpg' });
     res.end(data);
   })
@@ -53,7 +53,7 @@ router.get('/profile/:token', function(req, res, next) {
     .then(profle => {
       if(profle.length != 0){
         //프로필 이미지 보내기
-        fs.readFile(__dirname + `../../../profiles/${profle[0].fileName}`, function (err, data) {
+        fs.readFile(__dirname + `../../profiles/${profle[0].fileName}`, function (err, data) {
           res.writeHead(200, { 'Content-Type': 'image/jpg' });
           res.end(data);
         })
