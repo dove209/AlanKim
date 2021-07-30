7d9d48990d12d9a41a7030d4a08a2ea3855dcd35
dove209 <53247748+dove209@users.noreply.github.com>
2021-07-30 14:26:44 +0900

    Update imges.js

--- server/routes/api/imges.js
+++ server/routes/api/imges.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let router = express.Router();
 let fs = require('fs');
+let path = require('path');
 
 // Profile Model
 const Profile = require('../../models/profile');
@@ -22,7 +23,7 @@ const upload = multer({
 //남길랭 작성이 업로드한 사진 불러오기
 router.get('/:imgName', function (req, res) {
   let imgName = (req.params.imgName);
-  fs.readFile(__dirname + `../../uploads/${imgName}`, function (err, data) {
+  fs.readFile(path.join(__dirname, '../../uploads/', imgName ), function (err, data) {
     res.writeHead(200, { 'Content-Type': 'image/jpg' });
     res.end(data);
   })
@@ -50,10 +51,10 @@ router.put('/profile', upload.single("photo"), function (req, res, next) {
 router.get('/profile/:token', function(req, res, next) {
   let token = req.params.token.split('&')[0];
   Profile.find({ token: token }).sort({ upTime: -1})
-    .then(profle => {
-      if(profle.length != 0){
+    .then(profile => {
+      if(profile.length != 0){
         //프로필 이미지 보내기
-        fs.readFile(__dirname + `../../profiles/${profle[0].fileName}`, function (err, data) {
+        fs.readFile(path.join(__dirname, '../../profiles/', profile[0].fileName), function (err, data) {
           res.writeHead(200, { 'Content-Type': 'image/jpg' });
           res.end(data);
         })
